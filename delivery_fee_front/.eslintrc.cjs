3404337adc2db225b2bee247ad65008844cdc175
JerryU <jerry.uusitalo@gmail.com>
2024-06-10 10:33:58 +0300

    Update rules and lint

--- delivery_fee_front/.eslintrc.cjs
+++ delivery_fee_front/.eslintrc.cjs
@@ -2,26 +2,42 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/react",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: [
+    "react-refresh",
+    "react",
+    "react-hooks",
+    "jsx-a11y",
+    "@typescript-eslint",
+    "prettier",
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "prettier/prettier": true,
+    "@typescript-eslint/no-unused-vars": "warn",
+    "react/prop-types": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
-    "quotes": [
-      "error",
-      "double"
-    ],
-    "eqeqeq": "error",
-    "semi": [
-      "error",
-      "always"
-    ]
+    quotes: ["error", "double"],
+    eqeqeq: "error",
+    semi: ["error", "always"],
+    "object-curly-spacing": ["error", "always"],
   },
-}
+};
