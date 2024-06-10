ab2f8f413197947215e5d0c3251843cb26cf540b
JerryU <jerry.uusitalo@gmail.com>
2024-06-10 10:38:00 +0300

    Import Box

--- delivery_fee_front/src/App.tsx
+++ delivery_fee_front/src/App.tsx
@@ -1,11 +1,7 @@
 import Box from "@mui/material/Box";
-import { useState } from "react";
 
 function App() {
-  return (
-    <Box>
-    </Box>
-  );
+  return <Box></Box>;
 }
 
 export default App;
