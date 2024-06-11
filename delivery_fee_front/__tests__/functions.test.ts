74f942da8cdd95363f3eaf294b0555269b368f44
JerryU <jerry.uusitalo@gmail.com>
2024-06-11 15:13:32 +0300

    Add test cases for calculateSmallCartSurcharge()

--- delivery_fee_front/__tests__/functions.test.ts
+++ delivery_fee_front/__tests__/functions.test.ts
@@ -1,5 +1,43 @@
 import { distanceTestCases } from "./testHelper";
-import { calculateDistancePrice, calculateItemsPrice } from "../utils/library";
+import {
+  calculateSmallCartSurcharge,
+  calculateDistancePrice,
+  calculateItemsPrice,
+} from "../utils/library";
+
+describe("calculateSmallCartSurcharge()", () => {
+  describe("cart value is less than 10", () => {
+    for (let i = 0; i < 9; i++) {
+      test(`cart value ${i} should return ${10 - i}`, () => {
+        expect(calculateSmallCartSurcharge(i)).toEqual(10 - i);
+      });
+    }
+  });
+  describe("should return correct decimals", () => {
+    test("with value 9.99", () => {
+      expect(calculateSmallCartSurcharge(9.99)).toEqual(0.01);
+    });
+    test("with value 6.66", () => {
+      expect(calculateSmallCartSurcharge(6.66)).toEqual(3.34);
+    });
+    test("with value 3.14", () => {
+      expect(calculateSmallCartSurcharge(3.14)).toEqual(6.86);
+    });
+    test("with value 5.55", () => {
+      expect(calculateSmallCartSurcharge(5.55)).toEqual(4.45);
+    });
+    test("with value 0.01", () => {
+      expect(calculateSmallCartSurcharge(0.01)).toEqual(9.99);
+    });
+  });
+  describe("cart value is more 10 or more", () => {
+    for (let i = 10; i < 15; i++) {
+      test(`cartValue ${i} should return 0`, () => {
+        expect(calculateSmallCartSurcharge(i)).toEqual(0);
+      });
+    }
+  });
+});
 
 describe("calculateDistancePrice()", () => {
   distanceTestCases.forEach(({ distance, expected }) => {
