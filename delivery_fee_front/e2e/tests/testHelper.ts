373d2eed429708feb78eb7050dbb3c7752a3d8ec
JerryU <jerry.uusitalo@gmail.com>
2024-06-19 11:31:40 +0300

    Add clearDateTimeInput

--- delivery_fee_front/e2e/tests/testHelper.ts
+++ delivery_fee_front/e2e/tests/testHelper.ts
@@ -3,3 +3,13 @@ import { Page } from "@playwright/test";
 export const getByDataTestId = (value: string, page: Page) => {
   return page.locator(`[data-test-id="${value}"]`);
 };
+
+// Have different button combination for macOs
+export const clearDateTimeInput = async (page: Page) => {
+  if (process.platform === "darwin") {
+    await page.keyboard.press("Meta+A");
+  } else {
+    await page.keyboard.press("Control+A");
+  }
+  page.keyboard.press("Backspace");
+};
