40de4f326a3bab8bca2b5574219853014887f120
JerryU <jerry.uusitalo@gmail.com>
2024-06-19 10:22:25 +0300

    Add test cases for input behaviour

---
+++ delivery_fee_front/e2e/tests/main.test.ts
@@ -0,0 +1,76 @@
+import { test, expect, Locator } from "@playwright/test";
+import { getByDataTestId } from "./testHelper";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:5173");
+});
+// Have dev server running in localhost:5173
+test.describe("delivery fee calculator app", () => {
+  test("should have title and inputs are visible", async ({ page }) => {
+    const title = page.getByText("Delivery fee calculator");
+    const cartInput = getByDataTestId("cartValue", page);
+    const itemsInput = getByDataTestId("numberOfItems", page);
+    const deliveryInput = getByDataTestId("deliveryDistance", page);
+    const timeInput = getByDataTestId("orderTime", page);
+
+    await expect(title).toBeVisible();
+    await expect(cartInput).toBeVisible();
+    await expect(itemsInput).toBeVisible();
+    await expect(deliveryInput).toBeVisible();
+    await expect(timeInput).toBeVisible();
+  });
+  test.describe("cartValue input", () => {
+    let cartInput: Locator;
+    test.beforeEach(({ page }) => {
+      cartInput = getByDataTestId("cartValue", page);
+    });
+    test("initial value is empty string", async () => {
+      await expect(cartInput).toHaveValue("");
+    });
+    test("should accept floating point number separated with a comma", async ({
+      page,
+    }) => {
+      await cartInput.click();
+      await page.keyboard.type("10,22");
+      await expect(cartInput).toHaveValue("10.22");
+    });
+    test("should not accept invalid characters", async ({ page }) => {
+      await cartInput.click();
+      // Loop through Unicode code points for lowercase alphabets
+      const characters = "abcdefghiklmnopqrstuwxyzöäå-";
+      for (const character of characters) {
+        await page.keyboard.type(character);
+      }
+      await expect(cartInput).toHaveValue("");
+    });
+  });
+  test.describe("numberOfItems input", () => {
+    let itemsInput: Locator;
+    test.beforeEach(({ page }) => {
+      itemsInput = getByDataTestId("numberOfItems", page);
+    });
+    test("initial value is empty string", async () => {
+      await expect(itemsInput).toHaveValue("");
+    });
+    test("should accept integer number", async ({ page }) => {
+      await itemsInput.click();
+      await page.keyboard.type("10");
+      await expect(itemsInput).toHaveValue("10");
+    });
+    test("should not accept floating point number", async ({ page }) => {
+      await itemsInput.click();
+      await page.keyboard.type("10,22");
+      await expect(itemsInput).toHaveValue("1022");
+      await expect(itemsInput).not.toHaveValue("10.22");
+    });
+    test("should not accept invalid characters", async ({ page }) => {
+      await itemsInput.click();
+      // Loop through Unicode code points for lowercase alphabets
+      const characters = "abcdefghiklmnopqrstuwxyzöäå,.-";
+      for (const character of characters) {
+        await page.keyboard.type(character);
+      }
+      await expect(itemsInput).toHaveValue("");
+    });
+  });
+});
