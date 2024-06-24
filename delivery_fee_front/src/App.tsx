9ebc1f6cf040c3922ac3370c36fb3e49e49a567c
JerryU <jerry.uusitalo@gmail.com>
2024-06-24 19:20:56 +0300

    Add error handling if input values are 0

--- delivery_fee_front/src/App.tsx
+++ delivery_fee_front/src/App.tsx
@@ -6,6 +6,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import NumberInput from "./components/NumberInput";
+import AlertHandler from "./components/AlertHandler";
 import React, { useState } from "react";
 import {
   calculateItemsPrice,
@@ -14,6 +15,7 @@ import {
   calculateSmallCartSurcharge,
 } from "../utils/library";
 import DateTimeInput from "./components/DateTimeInput";
+import { SeverityTypes, InputError } from "../utils/types";
 
 function App() {
   const [cart, setCart] = useState<number>(0);
@@ -22,20 +24,54 @@ function App() {
   const [date, setDate] = useState<Dayjs>(dayjs());
   const [isRushHour, setIsRushHour] = useState<boolean>(false);
   const [deliveryFee, setDeliveryFee] = useState<number>(0);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [severity, setSeverity] = useState<SeverityTypes>("error");
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [inputError, setInputError] = useState<InputError>({
+    cartInput: false,
+    itemInput: false,
+    distanceInput: false,
+  });
 
+  // If input's value (or state) is 0 or less then display AlertHandler
+  // Else calculate total of the states
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    const itemsPrice = calculateItemsPrice(items);
-    const distancePrice = calculateDistancePrice(distance);
-    const smallCartSurcharge = calculateSmallCartSurcharge(cart);
-    const total = calculateDeliveryFee(
-      cart,
-      smallCartSurcharge,
-      distancePrice,
-      itemsPrice,
-      isRushHour
-    );
-    setDeliveryFee(total);
+    checkInputErrors();
+
+    if (Object.values(inputError).includes(true)) {
+      handleAlert("Please enter missing value", "error");
+    } else {
+      const itemsPrice = calculateItemsPrice(items);
+      const distancePrice = calculateDistancePrice(distance);
+      const smallCartSurcharge = calculateSmallCartSurcharge(cart);
+      const total = calculateDeliveryFee(
+        cart,
+        smallCartSurcharge,
+        distancePrice,
+        itemsPrice,
+        isRushHour
+      );
+      setDeliveryFee(total);
+    }
+  };
+
+  const handleAlert = (message: string, severity: SeverityTypes) => {
+    setErrorMessage(message);
+    setSeverity(severity);
+    setIsVisible(true);
+  };
+
+  // If input's value is equal or less than 0, set the value to true
+  // The output will be set to input's error prop
+  const checkInputErrors = () => {
+    const errors: InputError = {
+      cartInput: cart <= 0,
+      itemInput: items <= 0,
+      distanceInput: distance <= 0,
+    };
+    setInputError(errors);
+    return errors;
   };
 
   return (
@@ -51,6 +87,12 @@ function App() {
           gap: "1rem",
         }}
       >
+        <AlertHandler
+          message={errorMessage}
+          setIsVisible={setIsVisible}
+          isVisible={isVisible}
+          severity={severity}
+        />
         <Typography variant="h4" component={"h1"}>
           Delivery fee calculator
         </Typography>
@@ -64,18 +106,21 @@ function App() {
             setState={setCart}
             isFloatValue={true}
             dataTestId="cartValue"
+            hasError={inputError.cartInput}
           />
           <NumberInput
             label="Number of items (pcs)"
             setState={setItems}
             isFloatValue={false}
             dataTestId="numberOfItems"
+            hasError={inputError.itemInput}
           />
           <NumberInput
             label="Delivery Distance (m)"
             setState={setDistance}
             isFloatValue={false}
             dataTestId="deliveryDistance"
+            hasError={inputError.distanceInput}
           />
           <DateTimeInput
             setIsRushHour={setIsRushHour}
@@ -86,7 +131,9 @@ function App() {
             Calculate delivery fee
           </Button>
         </Box>
-        <Typography>Delivery fee: {deliveryFee}e</Typography>
+        <Typography>
+          Delivery fee: <span data-test-id="deliveryFee">{deliveryFee}</span>e
+        </Typography>
       </Box>
     </LocalizationProvider>
   );
