1c12d6e47a8cb9c5529f6a7f1cb2ccf6a78d7735
JerryU <jerry.uusitalo@gmail.com>
2024-06-24 19:52:45 +0300

    Fix right label text test

--- delivery_fee_front/__tests__/NumberInput.test.tsx
+++ delivery_fee_front/__tests__/NumberInput.test.tsx
@@ -13,6 +13,7 @@ describe("<NumberInput />", () => {
       setState={setStateMock}
       isFloatValue={true}
       dataTestId="test-id"
+      hasError={false}
     />
   );
   beforeEach(() => {
@@ -53,7 +54,13 @@ describe("<NumberInput />", () => {
     expect(input).toHaveValue(10);
   });
   test("should have right label text", () => {
-    const label = screen.getByText("test label");
+    // InputLabel component has one label and span element which both have the same text content
+    // This custom query returns the label element
+    const label = screen.getByText((content, element) => {
+      return (
+        element?.tagName.toLowerCase() === "label" && content === "test label"
+      );
+    });
     expect(label).toBeInTheDocument();
     expect(label).toHaveTextContent("test label");
   });
