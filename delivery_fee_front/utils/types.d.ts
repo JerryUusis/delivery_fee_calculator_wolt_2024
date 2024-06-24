ffa06e5d38ee3086a8c5666ac60ff3020f22d3b6
JerryU <jerry.uusitalo@gmail.com>
2024-06-24 19:21:33 +0300

    Add types

---
+++ delivery_fee_front/utils/types.d.ts
@@ -0,0 +1,7 @@
+export type SeverityTypes = "error" | "success" | "info" | "warning";
+
+export interface InputError {
+    cartInput: boolean;
+    itemInput: boolean;
+    distanceInput: boolean;
+}
