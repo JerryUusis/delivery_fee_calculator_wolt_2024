7081288e412e64227df74f13a00f87330eea1019
JerryU <jerry.uusitalo@gmail.com>
2024-06-24 20:37:38 +0300

    Add addValueToInput and addValuesToInputs functions

--- delivery_fee_front/e2e/tests/testHelper.ts
+++ delivery_fee_front/e2e/tests/testHelper.ts
@@ -1,4 +1,4 @@
-import { Page } from "@playwright/test";
+import { Page, Locator } from "@playwright/test";
 
 export const getByDataTestId = (value: string, page: Page) => {
   return page.locator(`[data-test-id="${value}"]`);
@@ -12,4 +12,35 @@ export const clearDateTimeInput = async (page: Page) => {
     await page.keyboard.press("Control+A");
   }
   page.keyboard.press("Backspace");
+};
+
+export const addValueToInput = async (
+  input: Locator,
+  value: string,
+  page: Page
+) => {
+  await input.click();
+  await page.keyboard.type(value);
+};
+
+export const addValuesToInputs = async (
+  cartValue: string,
+  itemsAmount: string,
+  distanceLength: string,
+  orderTime: string,
+  page: Page
+) => {
+  const cartInput = getByDataTestId("cartValue", page);
+  const itemsInput = getByDataTestId("numberOfItems", page);
+  const distanceInput = getByDataTestId("deliveryDistance", page);
+  const orderTimeInput = getByDataTestId("orderTime", page);
+  const submitButton = page.getByRole("button", {
+    name: "Calculate delivery fee",
+  });
+
+  await addValueToInput(cartInput, cartValue, page);
+  await addValueToInput(itemsInput, itemsAmount, page);
+  await addValueToInput(distanceInput, distanceLength, page);
+  await addValueToInput(orderTimeInput, orderTime, page);
+  await submitButton.click();
 };
