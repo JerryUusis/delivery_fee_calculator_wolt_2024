c29942d23dc17fc0d3c850815ca06d2bec41ac72
JerryU <jerry.uusitalo@gmail.com>
2024-06-24 20:39:30 +0300

    Add state to contorl error prop

--- delivery_fee_front/src/components/NumberInput.tsx
+++ delivery_fee_front/src/components/NumberInput.tsx
@@ -2,13 +2,13 @@ import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import { handleNumberInput } from "../../utils/library";
 import OutlinedInput from "@mui/material/OutlinedInput";
+import { useState } from "react";
 
 interface TextFieldProps {
   label: string;
   setState: React.Dispatch<React.SetStateAction<number>>;
   isFloatValue: boolean;
   dataTestId: string;
-  hasError: boolean;
 }
 
 const NumberInput = ({
@@ -16,8 +16,8 @@ const NumberInput = ({
   setState,
   isFloatValue,
   dataTestId,
-  hasError,
 }: TextFieldProps) => {
+  const [hasError, setHasError] = useState<boolean>(false);
   const setNumberType = (isFloatValue: boolean): {} => {
     if (isFloatValue) {
       return { min: 0, step: 0.01 };
@@ -38,6 +38,11 @@ const NumberInput = ({
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     handleNumberInput(e.target.value, setState, isFloatValue);
+    if (e.target.value === "0" || "") {
+      setHasError(true);
+    } else {
+      setHasError(false);
+    }
   };
 
   return (
